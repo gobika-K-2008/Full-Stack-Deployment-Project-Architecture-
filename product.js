994e13c6bf061e334798349208da0e6db1cc6857
gobika-K-2008 <gobikaageetha@gmail.com>
2026-06-24 19:22:56 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,37 @@
+export const products = [
+{
+id:1,
+name:"Laptop",
+price:65000,
+category:"Electronics",
+image:"https://picsum.photos/300?1",
+description:"High-performance laptop"
+},
+
+{
+id:2,
+name:"Headphones",
+price:2500,
+category:"Electronics",
+image:"https://picsum.photos/300?2",
+description:"Wireless headphones"
+},
+
+{
+id:3,
+name:"Shoes",
+price:3000,
+category:"Fashion",
+image:"https://picsum.photos/300?3",
+description:"Comfortable running shoes"
+},
+
+{
+id:4,
+name:"Smart Watch",
+price:5000,
+category:"Electronics",
+image:"https://picsum.photos/300?4",
+description:"Advanced smartwatch"
+}
+];
